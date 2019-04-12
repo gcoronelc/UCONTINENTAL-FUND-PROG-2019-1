a68e57bf14ea38dfed54ec1ad714c92ac6918e15
Alumno UC <soporte@ULO80PA403011.continental.edu.pe>
2019-04-12 09:59:50 -0500

    Sem 04

--- Sem04/AppVenta/src/pe/ucontinantal/appventa/AppVenta.java
+++ Sem04/AppVenta/src/pe/ucontinantal/appventa/AppVenta.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pe.ucontinantal.appventa;
 
-/**
- *
- * @author aulas_limno
- */
+import pe.ucontinantal.appventa.view.VentaView;
+
 public class AppVenta {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    VentaView.main(args);
   }
   
 }
