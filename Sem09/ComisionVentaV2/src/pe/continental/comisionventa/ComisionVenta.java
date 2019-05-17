099a0e94dfaad5e6b8492e59117a9f670b7500e1
Lima Los Olivos <aulas_limno@continental.edu.pe>
2019-05-17 09:11:32 -0500

    Parcial V2

---
+++ Sem09/ComisionVentaV2/src/pe/continental/comisionventa/ComisionVenta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.continental.comisionventa;
+
+import pe.continental.comisionventa.view.ComisionVentaView;
+
+/**
+ *
+ * @author soporte
+ */
+public class ComisionVenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ComisionVentaView.main(args);
+    }
+    
+}
---
+++ Sem09/ComisionVentaV2/src/pe/continental/comisionventa/service/VentaService.java
@@ -0,0 +1,122 @@
+package pe.continental.comisionventa.service;
+
+public class VentaService {
+
+  // Constantes
+  public static final String CAT_ADMINISTRACION = "ADMINISTRACION";
+  public static final String CAT_OFIMATICA = "OFIMATICA";
+  public static final String CAT_PROGRAMACION = "PROGRAMACION";
+  public static final String CAT_OTROS = "OTROS";
+
+  public static double determinarTotal(String categoria, int participante) {
+    // Variable
+    double total = 0.0;
+    // Proceso
+    switch (categoria.toUpperCase()) {
+      case CAT_ADMINISTRACION:
+        total = Administracion.determinarTotal(participante);
+        break;
+      case CAT_OFIMATICA:
+        total = Ofimatica.determinarTotal(participante);
+        break;
+      case CAT_PROGRAMACION:
+        total = Programacion.determinarTotal(participante);
+        break;
+      case CAT_OTROS:
+        total = Otros.determinarTotal(participante);
+        break;
+    }
+    // Reporte
+    return total;
+  }
+
+  public static double determinarImporte(String categoria, int participante) {
+    // VARIABLE 
+    double importe = 0.0;
+    // PROCESO
+    switch (categoria.toUpperCase()) {
+      case CAT_ADMINISTRACION:
+        importe = Administracion.determinarImporte(participante);
+        break;
+      case CAT_OFIMATICA:
+        importe = Ofimatica.determinarImporte(participante);
+        break;
+      case CAT_PROGRAMACION:
+        importe = Programacion.determinarImporte(participante);
+        break;
+      case CAT_OTROS:
+        importe = Otros.determinarImporte(participante);
+        break;
+    }
+    // REPORTE
+    return importe;
+
+  }
+
+  public static double determinarImpuesto(String categoria, int participante) {
+    // VARIABLE
+    double impuesto = 0.0;
+    // PROCESO
+    switch (categoria.toUpperCase()) {
+      case CAT_ADMINISTRACION:
+        impuesto = Administracion.determinarImpuesto(participante);
+        break;
+      case CAT_OFIMATICA:
+        impuesto = Ofimatica.determinarImpuesto(participante);
+        break;
+      case CAT_PROGRAMACION:
+        impuesto = Programacion.determinarImpuesto(participante);
+        break;
+      case CAT_OTROS:
+        impuesto = Otros.determinarImpuesto(participante);
+        break;
+    }
+    // REPORTE
+    return impuesto;
+  }
+
+  public static double determinarComision(String categoria,int participante) {
+    //VARIABLE
+    double comision = 0.0;
+    //PROCESO
+    switch (categoria.toUpperCase()) {
+      case CAT_ADMINISTRACION:
+        comision = Administracion.determinarComision(participante);
+        break;
+      case CAT_OFIMATICA:
+        comision = Ofimatica.determinarComision(participante);
+        break;
+      case CAT_PROGRAMACION:
+        comision = Programacion.determinarComision(participante);
+        break;
+      case CAT_OTROS:
+        comision = Otros.determinarComision(participante);
+        break;
+    }
+    //reporte
+    return comision;
+  }
+  
+  public static double determinarPrecio(String categoria) {
+    //VARIABLE
+    double precio = 0.0;
+    //PROCESO
+    switch (categoria.toUpperCase()) {
+      case CAT_ADMINISTRACION:
+        precio = Administracion.determinarPrecio(categoria);
+        break;
+      case CAT_OFIMATICA:
+        precio = Ofimatica.determinarPrecio(categoria);
+        break;
+      case CAT_PROGRAMACION:
+        precio = Programacion.determinarPrecio(categoria);
+        break;
+      case CAT_OTROS:
+        precio = Otros.determinarPrecio(categoria);
+        break;
+    }
+    //reporte
+    return precio;
+  }
+  
+}
